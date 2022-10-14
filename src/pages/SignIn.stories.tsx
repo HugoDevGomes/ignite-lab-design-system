bd7d15d1246e11fab60f1c636b8479663301a074
Hugo de Jesus Gomes <hugo-gomes@altamogiana.com.br>
2022-10-14 14:47:40 -0300

    Remove user event SignIn

--- src/pages/SignIn.stories.tsx
+++ src/pages/SignIn.stories.tsx
@@ -26,11 +26,6 @@ export const Default: StoryObj = {
     play: async ({ canvasElement }) => {
         const canvas = within(canvasElement)
 
-        userEvent.type(canvas.getByPlaceholderText('Digite seu e-mail'), 'hugogomeees@hotmail.com')
-        userEvent.type(canvas.getByPlaceholderText('**********'), '12345678')
-
-        userEvent.click(canvas.getByRole('button'))
-
         await waitFor(() => {
             return expect(canvas.getByText('Login Realizado!')).toBeInTheDocument()
         })    
