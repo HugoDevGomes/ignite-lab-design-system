7337c75048410332abfafc2289a16a30a3d5dc85
Hugo de Jesus Gomes <hugo-gomes@altamogiana.com.br>
2022-10-14 08:29:18 -0300

    change main.cjs configType

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -15,11 +15,11 @@ module.exports = {
   "features": {
     "storyStoreV7": true
   },
-  viteFinal: (config, { configtype } ) => {
-    if(configtype === 'PRODUCTION') {
+  viteFinal: (config, { configType } ) => {
+    if (configType === 'PRODUCTION') {
       config.base = '/ignite-lab-design-system/'
     }
 
     return config;
-  }
+  },
 }
